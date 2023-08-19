2abbc9c135806116a45d35d4dd70916bc7757c69
Masoumeh Rezai <you@example.com>
2023-08-20 03:20:55 +0430

    add button

--- src/Componet/AddCart.js
+++ src/Componet/AddCart.js
@@ -1,8 +1,10 @@
 import classes from './AddCart.module.css'
+import Button from './Button';
 const AddCart = (props) => {
     return (
         <div className={classes.cart}>
             <h1>Add to cart</h1>
+            <Button>Add me</Button>
 
 
         </div>
---
+++ src/Componet/Button.js
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return (
+        <div >
+            <button className="btn">
+            {props.children}
+            </button>
+        </div>
+    )
+}
+export default Button;
