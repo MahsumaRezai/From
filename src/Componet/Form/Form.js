137ca81d97edd0afc3ca3e96102791b869a58342
Masoumeh Rezai <you@example.com>
2023-08-20 07:54:30 +0430

    add input in form

--- src/Componet/Form/Form.js
+++ src/Componet/Form/Form.js
@@ -1,10 +1,17 @@
 import { Fragment } from 'react';
-import Cart from '../Cart/Cart'
+import Cart from '../Cart/Cart';
+import classes from './Form.module.css'
 const Form = (props) => {
     return (
         <Fragment>
             <Cart>
-                <h1>heee</h1>
+                <form className={classes.form}>
+                    <input placeholder="Name" />
+                    <input placeholder="Password" />
+
+
+
+                </form>
 
             </Cart>
         </Fragment>
